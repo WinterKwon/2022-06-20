74869161f458015aab70165e1a4a679a17c857c1
WinterKwon <winterkwon7017@gmail.com>
2022-06-20 10:37:07 +0900

    useState, useEffect로 db.json의 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
+function Header(){
+  return (<header>
+      <h1>
+        <a href='/'>WEB</a>
+      </h1>
+  </header>)
+}
+
+function Nav(){
+  return (
+    <nav>
+      <ol></ol>
+    </nav>
+  )
+
+}
 function App() {
+
+  const [topics, setTopics] = useState([]);
+  const refreshTopics = async ()=>{
+    const response = await fetch('http://localhost:3333/topics');
+    const result = await response.json();
+    setTopics(result)
+  }
+  useEffect(()=>{
+    refreshTopics()
+  },[])
   return (
     <div className="App">
+      <Header />
       Hello, World!
+      <Nav></Nav>
     </div>
   );
 }
